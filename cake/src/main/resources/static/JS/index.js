a918175ca8adf7ea3d3d9022bb9ed08704c9b7bc
markgasmidoQA <MGasmido@qa.com>
2022-04-21 13:21:25 +0100

    MG: created a script that loads the contents of the database onto the website on the initial load of the body

--- cake/src/main/resources/static/JS/index.js
+++ cake/src/main/resources/static/JS/index.js
@@ -35,8 +35,137 @@ const updateURL = document.querySelector("#updateCakeURLInput");
 const loadCakes = () => {
     axios
         .get(`${URL}/getAllCakes`)
-        .then()
+        .then(res => 
+            res.data.forEach(element => {
+                addCakeInitial(element);
+            }))
         .catch(err => console.log(err));
+}
+const addCakeInitial = (res) => {
+    const mainDiv = document.createElement("div");
+    mainDiv.setAttribute("class", "card cake");
+    mainDiv.setAttribute("style", "width: 18rem;");
+    mainDiv.setAttribute("id", `mainDiv${res.id}`);
+
+    //card image
+    const img = document.createElement("img");
+    img.setAttribute("src", `${res.cakeURL}`);
+    img.setAttribute("class", "card-img-top");
+    img.setAttribute("alt", `${res.cakeName}`);
+    img.setAttribute("id", `URL${res.id}`);
+
+    mainDiv.appendChild(img);
+
+    //card body
+    const bodyDiv = document.createElement("div");
+    bodyDiv.setAttribute("class", "card-body");
+
+    const h5 = document.createElement("h5");
+    h5.setAttribute("id", `cardCakeName${res.id}`)
+    const text = document.createTextNode(`${res.cakeName}`);
+
+    h5.appendChild(text);
+    bodyDiv.appendChild(h5);
+
+    const cardButton = document.createElement("button");
+    cardButton.setAttribute("type", "button");
+    cardButton.setAttribute("class", "btn btn-primary");
+    cardButton.setAttribute("data-bs-toggle", "modal");
+    cardButton.setAttribute("data-bs-target", `#Modal${res.id}`);
+    const text2 = document.createTextNode("Show More!");
+
+
+
+
+    cardButton.appendChild(text2);
+    bodyDiv.appendChild(cardButton);
+
+    //modal
+    const modalDiv = document.createElement("div");
+    modalDiv.setAttribute("class", "modal fade");
+    modalDiv.setAttribute("id", `Modal${res.id}`);
+    modalDiv.setAttribute("tabindex", "-1");
+    modalDiv.setAttribute("aria-labelledby", `ModalLabel${res.id}`);
+    modalDiv.setAttribute("aria-hidden", "true");
+
+    const modalDialogue = document.createElement("div");
+    modalDialogue.setAttribute("class", "modal-dialog modal-dialog-centered");
+
+    const modalContent = document.createElement("div");
+    modalContent.setAttribute("class", "modal-content");
+
+    const modalHeader = document.createElement("div");
+    modalHeader.setAttribute("class", "modal-header");
+
+    const modalId = document.createElement("h6");
+    modalId.setAttribute("id", `cakeId${res.id}`);
+    const text3 = document.createTextNode(`${res.id}`);
+    modalId.appendChild(text3);
+
+    const modalName = document.createElement("h5");
+    modalName.setAttribute("class", "modal-title");
+    modalName.setAttribute("id", `ModalLabel${res.id}`);
+
+    const text4 = document.createTextNode(`${res.cakeName}`);
+    const h52 = document.createElement("h5");
+    h52.setAttribute("id", `h52${res.id}`);
+    h52.appendChild(text4)
+
+    modalName.appendChild(modalId);
+    modalName.appendChild(h52);
+    modalHeader.appendChild(modalName);
+
+    const closeButton = document.createElement("button");
+    closeButton.setAttribute("type", "button");
+    closeButton.setAttribute("class", "btn-close");
+    closeButton.setAttribute("data-bs-dismiss", "modal");
+    closeButton.setAttribute("aria-label", "Close");
+
+    modalHeader.appendChild(closeButton);
+    modalContent.appendChild(modalHeader);
+
+    const modalBody = document.createElement("div");
+    modalBody.setAttribute("class", "modal-body");
+    modalBody.setAttribute("id", `modal-body${res.id}`);
+
+    const text5 = document.createTextNode(`${res.cakeDescription}`);
+
+    modalBody.appendChild(text5);
+    modalContent.appendChild(modalBody);
+
+    const modalFooter = document.createElement("div");
+    modalFooter.setAttribute("class", "modal-footer");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.setAttribute("class", "btn btn-danger");
+    deleteButton.setAttribute("data-bs-dismiss", "modal");
+    deleteButton.setAttribute("id", "deleteButton");
+    deleteButton.setAttribute("onClick", `deleteCake(${res.id})`);
+
+
+    const text7 = document.createTextNode("Delete");
+    deleteButton.appendChild(text7);
+    modalFooter.appendChild(deleteButton);
+
+    const closeButton2 = document.createElement("button");
+    closeButton2.setAttribute("type", "button");
+    closeButton2.setAttribute("class", "btn btn-secondary");
+    closeButton2.setAttribute("data-bs-dismiss", "modal");
+    const text6 = document.createTextNode("Close");
+    closeButton2.appendChild(text6);
+    modalFooter.appendChild(closeButton2);
+
+
+
+
+    modalContent.appendChild(modalFooter);
+    modalDialogue.appendChild(modalContent);
+    modalDiv.appendChild(modalDialogue);
+    bodyDiv.appendChild(modalDiv);
+    mainDiv.appendChild(bodyDiv);
+    display.appendChild(mainDiv);
+
 }
 
 
